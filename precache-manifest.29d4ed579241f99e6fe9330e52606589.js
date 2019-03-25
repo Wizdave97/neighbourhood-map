a30f1efd1f5fede6d643f89b3b96c3172ff775d7
DavidSalami <wizdave97@gmail.com>
2019-03-25 21:38:29 +0100

    Updates

---
+++ precache-manifest.29d4ed579241f99e6fe9330e52606589.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d40ec101596d83f1c4c760c66f33528",
+    "url": "/neighbourhood-map/static/media/menu.1d40ec10.svg"
+  },
+  {
+    "revision": "d103543d2b03b8c7c24b",
+    "url": "/neighbourhood-map/static/js/runtime~main.eb5548ae.js"
+  },
+  {
+    "revision": "c7746c9c67ed33dbfdd1",
+    "url": "/neighbourhood-map/static/js/main.669b9446.chunk.js"
+  },
+  {
+    "revision": "32ff4bbd613002f6cc37",
+    "url": "/neighbourhood-map/static/js/2.ae694afe.chunk.js"
+  },
+  {
+    "revision": "c7746c9c67ed33dbfdd1",
+    "url": "/neighbourhood-map/static/css/main.9f22ba7f.chunk.css"
+  },
+  {
+    "revision": "daf714f1d21e1c119d8d31b464c5e99f",
+    "url": "/neighbourhood-map/index.html"
+  }
+];
